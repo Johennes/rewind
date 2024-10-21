4f42febad95ea25abd91f8f9c5511d8326f39409
Johannes Marbach <n0-0ne+github@mailbox.org>
2024-10-21 14:13:11 +0200

    Add filename linting

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,7 +1,9 @@
+mod lint_filenames;
 mod lint_license;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use lint_filenames::LintFilenamesArgs;
 use lint_license::LintLicenseArgs;
 
 #[derive(Parser)]
@@ -12,11 +14,13 @@ struct CliParser {
 
 #[derive(Subcommand)]
 enum CliCommand {
+    LintFilenames(LintFilenamesArgs),
     LintLicense(LintLicenseArgs),
 }
 
 fn main() -> Result<()> {
     match CliParser::parse().command {
+        CliCommand::LintFilenames(args) => args.run(),
         CliCommand::LintLicense(args) => args.run(),
     }
 }
